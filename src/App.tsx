5e596af0fff7e12c32532fa1c9a2674d187d2911
cahadley <chrishadleyks@gmail.com>
2026-05-06 10:40:33 -0500

    zoom

--- src/App.tsx
+++ src/App.tsx
@@ -68,7 +68,7 @@ type AppState = {
   lastWeekKey: string;
 };
 
-const VERSION = "v5.5.26b";
+const VERSION = "v5.5.26c";
 const STORAGE_KEY = "hadtieri_house_v21_clean";
 const BASE_POINTS = 5;
 const BATHROOM_POINTS = 2;
